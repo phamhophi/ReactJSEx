d7e176a97125d05d106116a512b83c4f31798d59
Pham Ho Phi <phamhophi05@gmail.com>
2022-07-13 00:51:08 +0700

    jhk

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardBody,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardTitle,
+} from "reactstrap";
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDish: null,
+    };
+  }
+
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish });
+  }
+
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <Card>
+          {/* Hiển thị hình ảnh món ăn */}
+          <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card onClick={() => this.onDishSelect(dish)}>
+            {/* Hiển thị hình ảnh món ăn */}
+            <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+
+            <CardImgOverlay>
+              {/* Hiển thị tên món ăn*/}
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">{menu}</div>
+        <div className="row">{this.renderDish(this.state.selectedDish)}</div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
